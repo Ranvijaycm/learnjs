2d747171f8f4e2168e0b4f36b80a8c86b812cad5
Ranvijaycm <rnvjtest1@gmail.com>
2025-02-28 22:18:08 +0530

    added new file guess with html and js

--- printnum.js
+++ printnum.js
@@ -62,7 +62,7 @@ console.log("doesnt matter, you are still going to play ");
 
 let gameNum = 10;
 let attempt =0;
-let guess;
+let guess = 0;
 while(guess != gameNum){
     guess = Number(prompt(`Guess The number. no of attempts done =${attempt}`));
     attempt = attempt + 1;
