f59664d004705e869a67cdfc9df7b3677c70997c
Ranvijaycm <rnvjtest1@gmail.com>
2025-02-28 22:20:01 +0530

    removed the guess program from printnum file

--- printnum.js
+++ printnum.js
@@ -56,25 +56,6 @@
 // }
 // console.log("rest all are odd");
 
-// guess the number
-console.log("Are you ready to play the game?");
-console.log("doesnt matter, you are still going to play ");
-
-let gameNum = 10;
-let attempt =0;
-let guess = 0;
-while(guess != gameNum){
-    guess = Number(prompt(`Guess The number. no of attempts done =${attempt}`));
-    attempt = attempt + 1;
-    if(guess == gameNum){
-        alert(`Congratulations, you got it right in ${attempt}`);
-            break;
-        }
-            else
-            console.log("wrong");
-
-
-    }
 
 
 
