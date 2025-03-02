20c3152030bc06492bfa60a598908eb5ebc8aafb
Ranvijaycm <rnvjtest1@gmail.com>
2025-03-02 11:44:48 +0530

    added forof html and forof js

---
+++ forof.js
@@ -0,0 +1,9 @@
+let heroes =["Ram", "shyam", "iron man", "ant man", "thor"];
+for (let disp of heroes)
+     {
+    console.log(disp);
+     }
+
+for (let el of heroes){
+    console.log(el.toUpperCase(), el.toLowerCase());
+}
