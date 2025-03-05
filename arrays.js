ae101bc0c54b923affe6615ed2981372b2a41e6e
Ranvijaycm <rnvjtest1@gmail.com>
2025-03-05 20:54:28 +0530

    attended l4

---
+++ arrays.js
@@ -0,0 +1,25 @@
+// let foodItems = ["potato", "cheese", "tomato"] ;
+// console.log(foodItems);
+
+// foodItems.push("gazar");
+// console.log(foodItems);
+
+//foodItems.pop(0);    pop does not remove index wise. it just removes from last.
+// foodItems.pop();
+// console.log(foodItems);
+
+// console.log(foodItems.toString());
+
+
+// let indianHero =["ramu", "shyamu"];
+// let hollywoodHero =["ironman", "antman"];
+
+// let heroes = indianHero.concat(hollywoodHero);
+
+// console.log(heroes);
+
+let marks =[22,55,44,66,7,99,88,33];
+console.log(marks);
+console.log(marks.splice(0,2,83,"ramu"));
+
+console.log(marks);
